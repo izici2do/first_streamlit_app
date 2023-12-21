154dd08d5d80a695bbed4bbe0cd00a017d5c4081
izici2do <154550643+izici2do@users.noreply.github.com>
2023-12-21 12:27:56 -0700

    Update streamlit_app.py
    added a function to enable user select fruit by name

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 import pandas
-my_first_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.setindex('Fruit')
 
 # Let's put a pick list here so they can pick the fruits they want to include
-streamlit.multiselect("Pick some fruits:", list(my_first_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #display the table on the page
-streamlit.dataframe(my_first_list)
+streamlit.dataframe(my_fruit_list)
 
